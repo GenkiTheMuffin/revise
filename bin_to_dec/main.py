73822f6f393a94c51bd096e8c92c4a0e1a605419
Genki <owlysk@gmail.com>
2023-09-29 09:43:17 +0200

    kys

--- bin_to_dec/main.py
+++ bin_to_dec/main.py
@@ -2,10 +2,10 @@ num = int(input("Zadaj cislo: "))
 power = 1
 sol = 0
 while num > 0:
-    b = num % 2  # get remainder after dividing by 2 (get  numeral)
+    b = num % 10  # get remainder after dividing by 2 (get  numeral)
     sol = (
         sol + b * power
     )  # add numeral to solution (power of ten since we need it backwards)
-    power = power * 10  # next power of 10
-    num = num // 2  #
+    power = power * 2  # next power of 10
+    num = num // 10  #
 print(sol)
