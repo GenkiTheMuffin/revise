3b9ad7ebb206c7e24c1fe696f9da6e2f8c6cbbe7
Genki <owlysk@gmail.com>
2023-10-26 16:19:47 +0200

    /

--- middle_num/main.py
+++ middle_num/main.py
@@ -1,20 +1,16 @@
-def stredne_cislo():
-    n = int(input("Zadaj cislo: "))
-    pocet_cifier = 0
-    temp_cislo = n
-    while temp_cislo != 0:
-        temp_cislo //= 10
-        pocet_cifier += 1
-
-    temp_cislo = n
-    middle_index = pocet_cifier//2
-
-    if pocet_cifier % 2 == 1:
-        for i in range(pocet_cifier, middle_index, -1):
-            temp_cislo //= 10
-        middle_digit = temp_cislo % 10
+def middle_num():
+    number = int(input("Enter a number: "))
+    temp_number = number
+    numeral_count = 0
+    while temp_number != 0:
+        temp_number //= 10
+        numeral_count += 1
+    temp_number = number
+    middle_index = numeral_count // 2
+    for _ in range(numeral_count - middle_index - 1):
+        temp_number //= 10
+    middle_digit = temp_number % 10
     print(middle_digit)
-    return middle_digit
 
 
-stredne_cislo()
+middle_num()
